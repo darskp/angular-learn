302fcff30d3423d56ccd43036b32f119f0733c21
darskp <darshanpatgar100@gmail.com>
2024-02-24 11:37:21 +0530

    Component Template Syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  // template:
+  //   `<h1> hi</h1>`,
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent {
-  componentName: string = "Angular";
-  name: string = "";
-
-  getName(e: any) {
-    console.log(e.target.value);
-    this.name = e.target.value;
-  }
-  handlClick() {
-   alert(this.name)
-  }
-
 }

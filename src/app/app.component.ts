4ee477b2ccde7208f249cd87dfe2f9bb6309df6d
darskp <darshanpatgar100@gmail.com>
2024-02-24 12:19:09 +0530

    Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   // template:
   //   `<h1> hi</h1>`,
@@ -11,4 +12,14 @@ import { RouterOutlet } from '@angular/router';
 })
 
 export class AppComponent {
+  name = "Darshan"
+  city = "Bangalore"
+  status = "Single"
+  isDisabled = false
+  users = [
+    { id: 1, name: "Darshan1", city: "Bangalore", status: "Single" },
+    { id: 2, name: "Darshan2", city: "Mysore", status: "Married" },
+    { id: 3, name: "Darshan3", city: "Bangalore", status: "Single" },
+  ]
+
 }

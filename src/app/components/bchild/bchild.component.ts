31930b3d3a8511c2b341be59fe8ba3db1b9d52e3
darskp <darshanpatgar100@gmail.com>
2024-02-25 15:28:18 +0530

    signals

--- src/app/components/bchild/bchild.component.ts
+++ src/app/components/bchild/bchild.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input, InputSignal, Signal, WritableSignal, input } from '@angular/core';
 import { CounterService } from '../../services/counter.service';
 
+
+function nameFormate(value: string) {
+  return "Hi! " + value;
+}
 @Component({
   selector: 'app-bchild',
   standalone: true,
@@ -10,7 +14,11 @@ import { CounterService } from '../../services/counter.service';
 })
 export class BchildComponent {
   constructor(public counterservice: CounterService) {
-
-
   }
+
+  // @Input({ alias: "name" }) username:string=""
+ username = input("",{alias:"name",transform: nameFormate });
+  // onButtonClick() {
+  //   this.username="Darshan Kumar";
+  // }
 }
--- src/app/services/counter.service.ts
+++ src/app/services/counter.service.ts
@@ -1,22 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Signal, WritableSignal, computed, effect, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CounterService {
-  private counter = 0;
+  private counter: WritableSignal<number> = signal(0);
+  private doubleCount: Signal<number> = computed(() => this.counter() * 2);
+
+  constructor() {
+    effect(() => {
+      console.log(`count==> ${this.counter}`);
+      console.log(`doublecount==> ${this.doubleCount}`);
+    })
+  }
+
   getCounter() {
-    return this.counter
+    return this.counter();
+  }
+  getdoublecount() {
+    return this.doubleCount();
   }
   increment() {
-    this.counter = this.counter + 1
+    this.counter.update(value => value + 1)
   }
   decrement() {
-    if (this.counter >= 1) {
-      this.counter = this.counter - 1;
+    if (this.counter() >= 1) {
+      this.counter.update(value => value - 1)
     }
   }
   reset() {
-    this.counter = 0;
+    this.counter.set(0);
   }
 }
